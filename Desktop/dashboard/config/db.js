ebc024fe650f7e471312e066f9f7db304f7872a3
Moekyaw97 <71075675+Moekyaw97@users.noreply.github.com>
2020-12-24 16:45:32 +0630

    Update db.js

--- Desktop/dashboard/config/db.js
+++ Desktop/dashboard/config/db.js
@@ -2,6 +2,6 @@
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
 module.exports = {
-  db: 'mongodb+srv://admin:admin@cluster0.fdbt3.mongodb.net/dashboard?retryWrites=true&w=majority'
+  db: 'mongodb'
   
 };
